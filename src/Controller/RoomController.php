acf188cd539168f608350dd229240566c674423e
SauroMatheo <sauromatheo@gmail.com>
2024-12-03 13:36:34 +0100

    Functionnality

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -6,18 +6,43 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-use App\Repository\MessageRepository;
+use Symfony\Bundle\SecurityBundle\Security;
+
+use App\Repository\RoomRepository;
 
 class RoomController extends AbstractController
 {
-    #[Route('/room/{id}', name: 'app_room')]
-    public function index(int $id, MessageRepository $messageRepo): Response
+    #[Route('/room/{roomId}', name: 'app_room')]
+    public function index(int $roomId, RoomRepository $roomRepo, Security $security): Response
     {
-        $messages = $messageRepo->findBy([
-            "room" => $id
-        ]);
+        $user = $security->getUser();
+
+        if ($user === null) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        
+        $room = $roomRepo->find($roomId);
+
+        $isMember = false;
+        $userIdentifier = $user->getUserIdentifier();
+        foreach ($room->getUsers() as $member) {
+            if ($user->getUserIdentifier() == $userIdentifier) {
+                $isMember = true;
+                break;
+            }
+        }
+        
+        if (!$isMember) {
+            return $this->redirectToRoute('app_home');
+        }
+
+        
+        $messages = $room->getMessages();
+
         return $this->render('room/index.html.twig', [
-            'id' => $id,
+            "roomId" => $roomId,
+            "roomName" => $room->getName(),
             "messages" => $messages
         ]);
     }
